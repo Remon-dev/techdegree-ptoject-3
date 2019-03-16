8462c3e47f8d54da01c6949145305c8dea198bde
Remon <remon.dev@icloud.com>
2019-03-16 17:46:04 +0100

    ”Job Role” section

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*****************************************
+Treehouse Techdegree:
+FSJS project 3 - Interactive For
+******************************************/
+/********
+Remon Tewodros
+*****/
+
+// focus on the first text field when the page first loads
+const name = document.querySelector('#name');
+$(document).ready(function () {
+    $('#name').focus();
+});
+
+
+
+$('#other-title').hide();
+
+$('#other').click(function () {
+    $('#other-title').show();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://www.tutorialspoint.com/What-is-document-ready-method-in-jQuery
